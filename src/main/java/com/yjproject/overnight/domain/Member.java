ce3fdaa0a1d2dfc228e31371d963328190ff2bb3
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-08-30 16:11:59 +0900

    Create Member

---
+++ src/main/java/com/yjproject/overnight/domain/Member.java
@@ -0,0 +1,24 @@
+package com.yjproject.overnight.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+    private String address;
+    private String phone;
+
+}
