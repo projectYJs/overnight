c1a59e878e45bc2d93ae4aafba7e9198111e0f46
Lece <lece619@gmail.com>
2022-08-30 14:19:30 +0900

    Update Test1.java

--- src/main/java/com/yjproject/overnight/test/Test1.java
+++ src/main/java/com/yjproject/overnight/test/Test1.java
@@ -2,5 +2,10 @@ package com.yjproject.overnight.test;
 
 public class Test1 {
   String name;
+<<<<<<< Updated upstream
   String name3;
+=======
+  String name2;
+  String hi;
+>>>>>>> Stashed changes
 }
