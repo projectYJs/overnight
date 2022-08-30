9c2c1ded98153d735dff2110e05f16d8383ff886
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-08-30 16:26:51 +0900

    add Items
    add Items commit

---
+++ src/main/java/com/yjproject/overnight/domain/item/Item.java
@@ -0,0 +1,20 @@
+package com.yjproject.overnight.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+}
