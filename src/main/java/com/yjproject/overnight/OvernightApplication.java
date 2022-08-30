e341c2f5213ca290f8fee76536560b081639961b
Lece <lece619@gmail.com>
2022-08-30 13:45:04 +0900

    Initial commit

---
+++ src/main/java/com/yjproject/overnight/OvernightApplication.java
@@ -0,0 +1,13 @@
+package com.yjproject.overnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvernightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvernightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjproject/overnight/OvernightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjproject.overnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvernightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
