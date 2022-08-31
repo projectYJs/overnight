3bc1e6ad7367eb9df5c5b2f166beeca3b909a9af
Lece <lece619@gmail.com>
2022-08-31 15:57:56 +0900

    샘플추가 테이블 생성

---
+++ src/main/java/com/yjproject/overnight/domain/item/Product.java
@@ -0,0 +1,28 @@
+package com.yjproject.overnight.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id @GeneratedValue
+    @Column(name = "product_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "oatmeal_item_id")
+    private Oatmeal oatmeal;
+
+    @ManyToOne
+    @JoinColumn(name = "yogurt_item_id")
+    private Yogurt yogurt;
+
+    @ManyToOne
+    @JoinColumn(name = "topping_item_id")
+    private Topping topping;
+}
