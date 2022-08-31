98c006a0e316c7d594fe3f713f7f8083bc25ee6e
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-08-31 17:04:33 +0900

    item, product Repository add

--- src/main/java/com/yjproject/overnight/domain/item/Item.java
+++ src/main/java/com/yjproject/overnight/domain/item/Item.java
@@ -1,5 +1,6 @@
 package com.yjproject.overnight.domain.item;
 
+import com.yjproject.overnight.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,5 +18,17 @@ public abstract class Item {
     private int calorie;
     private int price;
     private int stockQuantity;
+
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 
 }
