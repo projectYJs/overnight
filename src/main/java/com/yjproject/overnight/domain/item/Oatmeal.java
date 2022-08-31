32ed54690e4e83c7f362a9234d68ef942338acd4
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-08-31 16:13:57 +0900

    add enum annotation

--- src/main/java/com/yjproject/overnight/domain/item/Oatmeal.java
+++ src/main/java/com/yjproject/overnight/domain/item/Oatmeal.java
@@ -5,11 +5,15 @@ import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Entity
 @DiscriminatorValue("O")
 @Getter @Setter
 public class Oatmeal extends Item{
+
+    @Enumerated(EnumType.STRING)
     private OatmealType oatmealType;
 
 }
