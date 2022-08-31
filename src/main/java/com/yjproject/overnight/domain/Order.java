68ec1e4cf527adbd7d9ee1d3bad670d9db02fb60
Lece <lece619@gmail.com>
2022-08-31 16:27:56 +0900

    Mapping

--- src/main/java/com/yjproject/overnight/domain/Order.java
+++ src/main/java/com/yjproject/overnight/domain/Order.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -16,6 +18,21 @@ public class Order {
     @Id @GeneratedValue
     @Column(name = "order_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private String name;
     
--- src/main/java/com/yjproject/overnight/domain/OrderItem.java
+++ src/main/java/com/yjproject/overnight/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package com.yjproject.overnight.domain;
 
+import com.yjproject.overnight.domain.item.Product;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,14 @@ public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 
     private int orderPrice;
     private int count;
